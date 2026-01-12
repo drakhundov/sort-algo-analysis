02833e757f20ea29d95b732bd1740eac30fa5ce9
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2026-01-12 23:48:24 +0300

    Make exec time module more robust and less prone to logical errors.

--- src/exec_tm.cpp
+++ src/exec_tm.cpp
@@ -3,15 +3,26 @@
 #include <chrono>
 
 static std::chrono::steady_clock::time_point g_begin, g_end;
+static bool result_is_valid;
 
 void start_measuring_time() {
     g_begin = std::chrono::steady_clock::now();
+    if (result_is_valid) {
+        result_is_valid = false;
+    }
 }
 
 void stop_measuring_time() {
     g_end = std::chrono::steady_clock::now();
+    result_is_valid = true;
 }
 
 std::int64_t get_exec_time_ms() {
-    return std::chrono::duration_cast<std::chrono::milliseconds>(g_end - g_begin).count();
+    if (!result_is_valid) {
+        throw std::runtime_error("[get_exec_time_ms] execution time is invalid");
+    }
+    auto tmp = g_end - g_begin;
+    g_end = g_begin = std::chrono::steady_clock::now();
+    result_is_valid = false;
+    return std::chrono::duration_cast<std::chrono::milliseconds>(tmp).count();
 }
