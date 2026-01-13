9f7115635f3887abc35f3edbd26a85cfd8b4337c
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2026-01-13 19:01:32 +0300

    Implement quick select.

--- include/order.h
+++ include/order.h
@@ -4,3 +4,6 @@
 
 // Find k'th element of v.
 int medianOfMedians(const std::vector<int> &v, int k);
+
+// pivot == -1 (by default) => select pivot randomly
+int quickSelect(const std::vector<int> &v, int left, int right, int k);
--- src/order/QuickSelect.cpp
+++ src/order/QuickSelect.cpp
@@ -1,1 +1,27 @@
+#include <random>
+#include <vector>
 
+#include "order.h"
+
+int quickSelect(const std::vector<int>& v, int left, int right, int k) {
+  // Find the k'th value of the sorted 'v'.
+  if (left >= right) {
+    return -1;
+  }
+  static std::vector v_cp(v.begin(), v.end());
+  int pivot_i = left + rand() % (right - left + 1);
+  int last = left;
+  std::swap(v_cp[left], v_cp[pivot_i]);
+  for (int i = left + 1; i <= right; i++) {
+    if (v_cp[i] < v_cp[left]) {
+      std::swap(v_cp[++last], v_cp[i]);
+    }
+  }
+  std::swap(v_cp[left], v_cp[last]);
+  if (last == k) return v_cp[k];
+  if (k > last) {
+    return quickSelect(v, last + 1, right, k);
+  } else {
+    return quickSelect(v, left, last - 1, k);
+  }
+}
